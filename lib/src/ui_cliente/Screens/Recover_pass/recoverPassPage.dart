167707b2ee17100a8cc618752be0130ef52c0598
Diana Gutierrez <loribeth90@hotmail.com>
2020-08-12 20:36:35 -0500

    version_1

---
+++ lib/src/ui_cliente/Screens/Recover_pass/recoverPassPage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:veciapi/src/ui_cliente/Screens/Recover_pass/components/body.dart';
+
+class RecoverPassPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
---
+++ lib/src/ui_cliente/Screens/Welcome/welcomePage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:veciapi/src/ui_cliente/Screens/Welcome/components/body.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
